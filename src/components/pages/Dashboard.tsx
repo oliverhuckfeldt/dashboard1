2c4956aab32df2b42a42a6b1be9ef17d61c613d2
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2025-06-13 14:41:56 +0200

    Add Chart.js and react-chartjs libraries to dependencies and create a simple graph widget component with two test instances.

--- src/components/pages/Dashboard.tsx
+++ src/components/pages/Dashboard.tsx
@@ -1,4 +1,5 @@
 import SimpleValueWidget from "components/ui/SimpleValueWidget";
+import SimpleGraphWidget from "components/ui/SimpleGraphWidget";
 
 import "./Dashboard.scss";
 
@@ -7,6 +8,37 @@ const simpleValueWidgetData = [
   { title: "Memory allocation", value: "54%", icon: "Microchip" },
   { title: "Number of processes", value: "154", icon: "Cog" },
 ];
+
+const simpleGraphWidgetData = {
+  title: "Monthly Sales",
+  description: "Sales data for the last six months",
+  data: [
+    {
+      label: "Jan 25",
+      value: 49677,
+    },
+    {
+      label: "Feb 25",
+      value: 47451,
+    },
+    {
+      label: "Mar 25",
+      value: 52690,
+    },
+    {
+      label: "Apr 25",
+      value: 51406,
+    },
+    {
+      label: "May 25",
+      value: 47320,
+    },
+    {
+      label: "Jun 25",
+      value: 50258,
+    },
+  ],
+};
 
 export default function Dashboard() {
   return (
@@ -20,6 +52,18 @@ export default function Dashboard() {
             icon={card.icon}
           />
         ))}
+      </div>
+      <div className="row">
+        <SimpleGraphWidget
+          title={simpleGraphWidgetData.title}
+          description={simpleGraphWidgetData.description}
+          data={simpleGraphWidgetData.data}
+        />
+        <SimpleGraphWidget
+          title={simpleGraphWidgetData.title}
+          description={simpleGraphWidgetData.description}
+          data={simpleGraphWidgetData.data}
+        />
       </div>
     </>
   );
