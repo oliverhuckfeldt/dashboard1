8944b168cbee2254a71e1b398d94e5e99959c449
Oliver Huckfeldt <oliverhuckfeldt@gmail.com>
2025-06-16 11:59:34 +0200

    Remove Chart.js and add Victory Charts to dependencies. Create a simple line graph widget component.

--- src/components/ui/SimpleGraphWidget.tsx
+++ src/components/ui/SimpleGraphWidget.tsx
@@ -1,7 +1,11 @@
-import type { ChartData } from "chart.js";
-import Chart from "chart.js/auto";
-import { CategoryScale } from "chart.js";
-import { Line } from "react-chartjs-2";
+import {
+  VictoryChart,
+  VictoryTheme,
+  VictoryLine,
+  VictoryScatter,
+  VictoryTooltip,
+  VictoryAxis,
+} from "victory";
 
 import "./SimpleGraphWidget.scss";
 
@@ -11,48 +15,33 @@ interface SimpleGraphWidgetProps {
   data: { label: string; value: number }[];
 }
 
-Chart.register(CategoryScale);
-
-const options = {
-  animation: false as false,
-  responsive: true,
-  plugins: { legend: { display: false }, tooltip: { displayColors: false } },
-  scales: {
-    x: {
-      grid: { display: false },
-      border: { display: false },
-    },
-    y: {
-      ticks: { display: false },
-      grid: { display: false },
-      border: { display: false },
-    },
-  },
-};
-
 export default function SimpleGraphWidget({
   title,
   description,
   data,
 }: SimpleGraphWidgetProps) {
-  const chartData: ChartData<"line"> = {
-    labels: data.map((item) => item.label),
-    datasets: [
-      {
-        data: data.map((item) => item.value),
-        borderColor: "rgba(75,192,192,1)",
-        borderWidth: 4,
-        fill: false,
-        cubicInterpolationMode: "monotone",
-      },
-    ],
-  };
-
   return (
     <div className="simple-graph-widget">
       <h3>{title}</h3>
       <p>{description}</p>
-      <Line data={chartData} options={options} />
+      <VictoryChart theme={VictoryTheme.clean}>
+        <VictoryLine
+          data={data.map((item) => ({
+            x: item.label,
+            y: item.value,
+          }))}
+        />
+        <VictoryScatter
+          data={data.map((item) => ({
+            x: item.label,
+            y: item.value,
+          }))}
+          size={5}
+          labels={({ datum }) => datum.y}
+          labelComponent={<VictoryTooltip />}
+        />
+        <VictoryAxis />
+      </VictoryChart>
     </div>
   );
 }
